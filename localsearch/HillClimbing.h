558bc97b21293e0f00440b7a2020be78f49134bd
Jonas Tollenaere <jonas.tollenaere@kuleuven.be>
2022-06-26 16:57:25 +0200

    Relocating local search framework

---
+++ localsearch/HillClimbing.h
@@ -0,0 +1,37 @@
+//
+// Created by Jonas on 29/03/2022.
+//
+
+#ifndef OPTIXMESHCORE_HILLCLIMBING_H
+#define OPTIXMESHCORE_HILLCLIMBING_H
+
+#include "AbstractLocalSearch.h"
+
+template<class S>
+class HillClimbing: public AbstractLocalSearch<S>{
+private:
+    bool acceptEqual = false;
+
+public:
+    HillClimbing() = default;
+    explicit HillClimbing(bool acceptEqual): acceptEqual(acceptEqual){}
+private:
+    double performIteration(unsigned int currentIteration, std::shared_ptr<S> currentSolution, double currentScore) override {
+        auto move = this->moveFactory.sample(currentSolution, this->random);
+        move->doMove(currentSolution);
+        auto newScore = this->objectiveFunction.evaluate(currentSolution);
+        if(newScore < currentScore || (acceptEqual && newScore == currentScore)){
+            return newScore;
+        }
+        else{
+            move->undoMove(currentSolution);
+            return currentScore;
+        }
+    }
+
+    void initialize(std::shared_ptr<S> initialSolution, double initialScore) override {
+
+    }
+};
+
+#endif //OPTIXMESHCORE_HILLCLIMBING_H
