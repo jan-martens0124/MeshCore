2395621f998c07bef9adf955afcd73e9271a03a8
Jonas Tollenaere <jonas.tollenaere@kuleuven.be>
2022-09-18 10:55:33 +0200

    Fixed rotation order in OBB-OBB intersection

--- geometric/OBBOBB.cpp
+++ geometric/OBBOBB.cpp
@@ -20,27 +20,28 @@ namespace Intersection {
         const auto& firstAABB = firstOBB.getAabb();
         const auto& secondAABB = secondOBB.getAabb();
 
-        // First search for a separating axis aligned with first OBB
+        // First search for a separating axis that is aligned with first OBB
         {
-//            glm::mat4 secondToFirstTransformationMatrix = secondOBB.getRotation().getMatrix() * firstOBB.getRotation().getInverseMatrix();
-            Quaternion secondToFirstRotation = secondOBB.getRotation() * firstOBB.getRotation().getInverse();
-            glm::bvec3 secondComponentWiseGreater(true); // Stays true if the second OBB is separated (greater) on the corresponding axis (x, y or z)
-            glm::bvec3 secondComponentWiseSmaller(true); // Stays true if the second OBB is separated (smaller) on the corresponding axis (x, y or z)
+            Quaternion secondToFirstRotation = firstOBB.getRotation().getInverse() * secondOBB.getRotation();
+            glm::bvec3 secondComponentWiseGreater(true); // Stays true if the second OBB is separated on the corresponding axis (x, y or z) by being greater
+            glm::bvec3 secondComponentWiseSmaller(true); // Stays true if the second OBB is separated on the corresponding axis (x, y or z) by being smaller
 
-            // Iterate over all the corner points of the second OBB (checking these is enough)
+            // Iterate over all the corner points of the second OBB (checking these, is enough)
             Vertex extremePoints[] = {secondAABB.getMinimum(), secondAABB.getMaximum()};
             for(int xi=0; xi<=1; xi++){
                 for(int yi=0; yi<=1; yi++){
                     for(int zi=0; zi<=1; zi++){
 
-                        Vertex corner = secondToFirstRotation * extremePoints[xi];
+                        // Rotate the corner point of the second OBB to the first OBBs coordinate system
+                        Vertex rotatedCorner = secondToFirstRotation.rotateVertex({extremePoints[xi].x, extremePoints[yi].y, extremePoints[zi].z});
 
-                        secondComponentWiseGreater &= glm::greaterThan(corner, firstAABB.getMaximum());
-                        secondComponentWiseSmaller &= glm::lessThan(corner, firstAABB.getMinimum());
+                        // Check if the corner point is separated on each axis (x, y or z)
+                        secondComponentWiseGreater &= glm::greaterThan(rotatedCorner, firstAABB.getMaximum());
+                        secondComponentWiseSmaller &= glm::lessThan(rotatedCorner, firstAABB.getMinimum());
 
-                        if(!(glm::any(secondComponentWiseGreater) || glm::any(secondComponentWiseSmaller))){
-                            goto endFirstNestedLoop;
-                        }
+//                        if(!(glm::any(secondComponentWiseGreater) || glm::any(secondComponentWiseSmaller))){ // TODO test if this actually improves performance, OBBs can be separated most of the time
+//                            goto endFirstNestedLoop; // If the second OBB is not separated on any axis, we can stop the search
+//                        }
                     }
                 }
             }
@@ -48,30 +49,27 @@ namespace Intersection {
                 return false;
             }
         }
-        endFirstNestedLoop:
+//        endFirstNestedLoop:
 
-        // Then search for a separating axis aligned with the second OBB
+        // Then search for a separating axis that is aligned with the second OBB
         {
-            // TODO verify if the quaternion approach reaches the same result
-//            glm::mat4 firstToSecondTransformationMatrix = firstOBB.getRotation().getMatrix() * secondOBB.getRotation().getInverseMatrix();
-//
-            Quaternion firstToSecondRotation = firstOBB.getRotation() * secondOBB.getRotation().getInverse();
-            glm::bvec3 firstComponentWiseGreater(true); // Stays true if the first OBB is separated (greater) on the corresponding axis (x, y or z)
-            glm::bvec3 firstComponentWiseSmaller(true); // Stays true if the first OBB is separated (smaller) on the corresponding axis (x, y or z)
+            Quaternion firstToSecondRotation = secondOBB.getRotation().getInverse() * firstOBB.getRotation();
+            glm::bvec3 firstComponentWiseGreater(true); // Stays true if the first OBB is separated on the corresponding axis (x, y or z) by being greater
+            glm::bvec3 firstComponentWiseSmaller(true); // Stays true if the first OBB is separated on the corresponding axis (x, y or z) by being smaller
 
-            // Iterate over all the corner points of the second OBB (checking these is enough)
+            // Iterate over all the corner points of the second OBB (checking these, is enough)
             Vertex extremePoints[] = {firstAABB.getMinimum(), firstAABB.getMaximum()};
             for(int xi=0; xi<=1; xi++) {
                 for (int yi = 0; yi <= 1; yi++) {
                     for (int zi = 0; zi <= 1; zi++) {
 
-                        Vertex corner = firstToSecondRotation * extremePoints[xi];
-                        firstComponentWiseGreater &= glm::greaterThan(corner, secondAABB.getMaximum());
-                        firstComponentWiseSmaller &= glm::lessThan(corner, secondAABB.getMinimum());
+                        Vertex rotatedCorner = firstToSecondRotation.rotateVertex({extremePoints[xi].x, extremePoints[yi].y, extremePoints[zi].z});
+                        firstComponentWiseGreater &= glm::greaterThan(rotatedCorner, secondAABB.getMaximum());
+                        firstComponentWiseSmaller &= glm::lessThan(rotatedCorner, secondAABB.getMinimum());
 
-                        if(!(glm::any(firstComponentWiseGreater) || glm::any(firstComponentWiseSmaller))){
-                            goto endSecondNestedLoop;
-                        }
+//                        if(!(glm::any(firstComponentWiseGreater) || glm::any(firstComponentWiseSmaller))){ // TODO test if this actually improves performance, OBBs can be separated most of the time
+//                            goto endSecondNestedLoop;
+//                        }
                     }
                 }
             }
@@ -79,7 +77,7 @@ namespace Intersection {
                 return false;
             }
         }
-        endSecondNestedLoop:
+//        endSecondNestedLoop:
         return true;
     }
 }
